7d21e1eb6a7d12dbc96c5bf940d7f3da2f24a91f
Alexander Makedonski <alexanderm@prime-code.com>
2023-04-05 09:43:07 +0300

    up

--- src/app/main/login-scan/component.ts
+++ src/app/main/login-scan/component.ts
@@ -63,7 +63,7 @@ export class LoginPage implements OnInit, OnDestroy {
       ogUrl: 'https://www.znanieplus.bg/login',
       ogType: 'article',
       ogDescription: 'ЗНАНИЕ+ е първата социална придобивка в България, която дава възможност на работодателя да подпомогне културното обогатяване на своите служители чрез фиксиран месечен или годишен абонамент на разумна цена.',
-      ogImage: 'https://www.znanieplus.bg/assets/images/logo.png',
+      ogImage: 'https://www.znanieplus.bg/assets/images/FB_Znanie+_2000x2000_01.jpg',
       canonicalURL: '/login'
     });
     if (isPlatformBrowser(this.platform)) {
