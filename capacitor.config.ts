1d5bc4913bef201674e480b4dcb200ee7b6752f8
Alexander Makedonski <alexanderm@prime-code.com>
2023-10-14 23:32:23 +0300

    up

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'bg.znanieplus.app',
   appName: 'Знание плюс',
   webDir: 'dist/capacitor',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https',
     hostname: "znanaieplus"
